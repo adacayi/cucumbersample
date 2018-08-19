8668dcb150a0a57a80d9d6e30a82ad6abb295797
Abdullah Sanver <a_sanver@hotmail.com>
2018-08-19 16:39:32 +0100

    public access modifier added to LoginStepsJava8 again, because without it the step definition could not be found. WebDriver class is reverted to public as well.

--- src/test/java/com/sanver/learning/cucumbersample/LoginStepsJava8.java
+++ src/test/java/com/sanver/learning/cucumbersample/LoginStepsJava8.java
@@ -2,7 +2,7 @@ package com.sanver.learning.cucumbersample;
 
 import cucumber.api.java8.En;
 
-class LoginStepsJava8 implements En {
+public class LoginStepsJava8 implements En {
     public LoginStepsJava8() {
         And("^I set the Java version to \"([^\"]*)\"$", (String version) ->
                 System.out.printf("\nThe version of Java is set to %s", version));
--- src/test/java/com/sanver/learning/cucumbersample/WebDriver.java
+++ src/test/java/com/sanver/learning/cucumbersample/WebDriver.java
@@ -1,6 +1,6 @@
 package com.sanver.learning.cucumbersample;
 
-class WebDriver {
+public class WebDriver {
     private String name;
 
     String getName() {
